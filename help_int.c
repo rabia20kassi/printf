f9cf9eabb6f6d3558fe21c8f1d9f21630293a754
laila22haz <hazmirlaila316@gmail.com>
2023-04-25 14:06:36 -0700

    yarbi ikhedem

--- help_int.c
+++ help_int.c
@@ -11,18 +11,18 @@
  */
 void set_int(int a, int *i)
 {
-        if (a < 0)
-        {
-                _putchar('-');
-                (*i)++;
-                a = -a;
-        }
-        if (a / 10)
-        {
-                set_int(a / 10, i);
-        }
-        _putchar('0' + (a % 10));
-        (*i)++;
+	if (a < 0)
+	{
+		_putchar('-');
+		(*i)++;
+		a = -a;
+	}
+	if (a / 10)
+	{
+		set_int(a / 10, i);
+	}
+	_putchar('0' + (a % 10));
+	(*i)++;
 }
 
 /**
@@ -34,25 +34,23 @@ void set_int(int a, int *i)
 int help_int(va_list args)
 {
 
-        int a = va_arg(args, int);
-        int i = 0;
+	int a = va_arg(args, int);
+	int i = 0;
 
-        if (a == 0)
-        {
-                _putchar('0');
-                return (1);
-        }
+	if (a == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 
-        if (a == INT_MIN)
-        {
-                _putchar('-');
-                _putchar('2');
- set_int(147483648, &i);
-                i += 2;
-                return (i);
-        }
-        set_int(a, &i);
-        return (i);
+	if (a == INT_MIN)
+	{
+		_putchar('-');
+		_putchar('2');
+		set_int(147483648, &i);
+		i += 2;
+		return (i);
+	}
+	set_int(a, &i);
+	return (i);
 }
-
-
--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <limits.h>
 
 
 int _printf(const char *format, ...);
