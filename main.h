0b042266ebd0c1c2e0a839eb2f034ef313a6d806
rabia20kassi <rabiaakassi@gmail.com>
2023-04-25 21:50:57 +0100

    yarbi ikhdem

--- main.h
+++ main.h
@@ -15,6 +15,8 @@ int help_char(va_list args);
 int switch_help(char id, va_list args);
 int help_int(va_list args);
 int help_bin(va_list args);
+void set_int(int a, int *i);
+int help_int(va_list args);
 int help_uns(unsigned int a, char *c);
 
 
